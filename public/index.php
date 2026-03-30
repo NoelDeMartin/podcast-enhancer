804580bfe050531a5541ef0eccb7aad4cfde496c
Noel De Martin <noel@noeldemartin.com>
2026-03-30 12:39:01 +0200

    Remove memory limits

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ define('LARAVEL_START', microtime(true));
 
 ini_set('post_max_size', '500M');
 ini_set('upload_max_filesize', '500M');
-ini_set('memory_limit', '512M');
+ini_set('memory_limit', -1);
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
